cbbf67ec2c7c56f9c5a15059495620aacdb971ea
Muhammad Hamza Arshad <hamzagondal2002@gmail.com>
2024-04-23 12:17:31 +0500

    tuesday 23 2024

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React, { Suspense, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import {
@@ -8,34 +7,27 @@ import {
   Preload,
   useTexture,
 } from "@react-three/drei";
-import PropTypes from "prop-types"; // Import PropTypes
+
 import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
-  const mesh = useRef();
-  useFrame(() => {
-    mesh.current.rotation.y = mesh.current.rotation.x += 0.01;
-  });
+  // const mesh = useRef();
+  // useFrame(() => {
+  //   mesh.current.rotation.y = mesh.current.rotation.x += 0.01;
+  // });
   return (
-    // eslint-disable-next-line react/no-unknown-property
     <mesh position={[1, 0, 0]}>
       <sphereGeometry
-        // eslint-disable-next-line react/no-unknown-property
         attach="geometry"
-        // eslint-disable-next-line react/no-unknown-property
         args={[2.2, 22, 50]} // Width, Height and Depth of the sphere
-        // eslint-disable-next-line react/no-unknown-property
         smoothness={5}
         {...props}
       />
       <meshPhongMaterial
         color="#420faf"
-        // eslint-disable-next-line react/no-unknown-property
         attach="material"
-        // eslint-disable-next-line react/no-unknown-property
         roughness={0.1}
-        // eslint-disable-next-line react/no-unknown-property
         metalness={0.1}
       />
       <Decal
@@ -46,11 +38,6 @@ const Ball = (props) => {
       />
     </mesh>
   );
-};
-
-Ball.propTypes = {
-  imgUrl: PropTypes.string.isRequired, // Validate that imgUrl is a required string
-  icon: PropTypes.string.isRequired, // Validate that imgUrl is a required string
 };
 
 const BallCanvas = ({ icon }) => {
