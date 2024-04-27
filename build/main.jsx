86e1c45d264fb72067d85d8db83900853fb8a76f
Muhammad Hamza Arshad <hamzagondal2002@gmail.com>
2024-04-27 19:39:29 +0500

    saturday 27

---
+++ build/main.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { createRoot } from "react-dom";
+import App from "../src/App";
+import "../src/index.css";
+import "tailwindcss/tailwind.css";
+
+createRoot(document.getElementById("root")).render(<App />);
