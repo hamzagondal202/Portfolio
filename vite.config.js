98bbf140942b177c0f438452430a611d06656d14
Muhammad Hamza Arshad <hamzagondal2002@gmail.com>
2024-05-09 01:52:31 +0500

    9/5/24

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
   plugins: [react()],
-  server: {
-    port: 3003, // Change the port number here
-  },
-  build: {
-    outDir: "build",
-    rollupOptions: {
-      input: {
-        main: "/src/main.jsx", // Update the input entry point if necessary
-      },
-    },
-  },
   css: {
     preprocessorOptions: {
       scss: {
